0b7952f13033b68a8ec26eb93967d62d65eef764
Shinin Varongchayakul <shivarong@gmail.com>
2025-09-07 22:35:00 +0700

    Rename df to students_df

--- ch_06_data_manipulation_with_dplyr.R
+++ ch_06_data_manipulation_with_dplyr.R
@@ -1,8 +1,36 @@
 # Data manipulation with dplyr
 
 
+
+# Install dplyr
+install.packages("dplyr")
+
+# Load dplyr
+library(dplyr)
+
+
+# Pipe operator examples
+
+# Calculate mean of 1:10
+mean(1:10)
+
+# Calculate mean of 1:10
+1:10 |> mean()
+
+# Calculate mean of 1:10 (round to 0)
+round(mean(1:10), 0)
+
+# Calculate mean of 1:10 (round to 0)
+1:10 |>
+  mean() |>
+  round(0)
+
+
+# ------------------------------------------------------------------
+
+
 # Create a data frame
-df <- data.frame(
+students_df <- data.frame(
   name = c("Anna", "Ben", "Chai", "Dana", "Eli",
            "Fah", "Gina", "Hiro", "Ivan", "Jin"),
   gender = c("F", "M", "M", "F", "M",
@@ -15,57 +43,56 @@ df <- data.frame(
                  68, 90, 82, 79, 74)
 )
 
-# View the df
-df
+# view the df
+students_df
 
 
-# -------------------------------------------------
+# 1. select()
+
+# Select name and stress_level
+students_df |> select(name, stress_level)
 
 
-# Select
-df |> select(name, stress_level)
+# 2. filter()
+
+# Filter for stress_level is "High"
+students_df |> filter(stress_level == "High")
+
+# Filter for males with stress_score greater than 5
+students_df |> filter(gender == "M" & stress_score > 5)
 
 
-# -------------------------------------------------
+# 3. arrange()
+
+# Sort by stress_score
+students_df |> arrange(stress_score)
+
+# Sort by stress_score (descending)
+students_df |> arrange(desc(stress_score))
 
 
-# Filter
-df |> filter(stress_level == "High")
+# (4) summarise()
 
-df |> filter(gender == "M" & stress_score > 5)
-
-
-# -------------------------------------------------
-
-
-# Arrange
-df |> arrange(desc(stress_score))
-
-
-# -------------------------------------------------
-
-
-# Summarise
-df |>
+# Summarise mean for stress_score and exam_score
+students_df |>
   summarise(mean_stress = mean(stress_score),
             mean_exam = mean(exam_score))
 
-# Summarise
-df |>
+# Summarise mean for stress_score and exam_score by gender
+students_df |>
   
   # Group by gender
   group_by(gender) |>
   
   # Calculate mean
   summarise(mean_stress = mean(stress_score),
-            mean_exam = mean(exam_score)) |>
-  
-  # Ungroup data
-  ungroup()
+            mean_exam = mean(exam_score))
 
 
-# Mutate
-df |>
+# 5. mutate()
+
+# Create column grade
+students_df |>
   mutate(grade = case_when(
     exam_score >= 90 ~ "A",
     exam_score >= 80 ~ "B",
@@ -75,11 +102,10 @@ df |>
   ))
 
 
-# -------------------------------------------------
+# Put It All Together
 
-
-# Together 1
-df |>
+# dplyr verbs together 1
+students_df |>
   
   # Group by stress level
   group_by(stress_level) |>
@@ -90,23 +116,20 @@ df |>
   # Sort by mean, descending
   arrange(desc(mean_exam))
 
-
-# Together 2
-df |>
+# dplyr verbs together 2
+students_df |>
   
   # Filter for high stress level and exam score below mean
-  filter(stress_level == "High" & 
-           exam_score < mean(exam_score)) |>
+  filter(stress_level == "High" & exam_score < mean(exam_score)) |>
   
   # Select only relevant columns
   select(name, gender, stress_score, exam_score) |>
   
   # Sort by stress score, descending
-  arrange(desc(stress_score)) 
+  arrange(desc(stress_score))
 
-
-# Together 3
-df |>
+# dplyr verbs together 3
+students_df |>
   
   # Create a performance index from exam and stress scores
   mutate(performance_index = exam_score / stress_score) |>
