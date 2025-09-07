9e734401437228b21019412bcd569d40c6e51707
Shinin Varongchayakul <shivarong@gmail.com>
2025-09-07 12:36:42 +0700

    Change example to sat.act dataset

--- ch_11_linear_regression.R
+++ ch_11_linear_regression.R
@@ -1,47 +1,23 @@
 # Linear regression
 
 
-# Set seed for reproducibility
-set.seed(123)
+# Load packages
+library(psych)
+library(dplyr)
 
-# Create a data frame
-sleep_df <- data.frame(
-  
-  # ID
-  id = 1:30,
-  
-  # Exercise: hours/week
-  exercise = round(rnorm(30, mean = 4, sd = 2), 1),
-  
-  # Caffeine intake: mg/day
-  caffeine = round(rnorm(30, mean = 200, sd = 80), 0),
-  
-  # Screen time: hours/day
-  screen_time = round(rnorm(30, mean = 6, sd = 2), 1),
-  
-  # Sleep: hours/day
-  sleep = round(rnorm(30, mean = 7, sd = 1), 1)
-)
+# Load the dataset
+data(sat.act)
 
-# View the df
-regression_df
-
-
-# -------------------------------------------------------
-
-
-# Perform a simple linear regression analysis
-lm_1 <- lm(sleep ~ screen_time, data = sleep_df)
+# Select predictors and outcome
+predict_score_df <- sat.act |>
+  select(age, education, SATV, SATQ, ACT)
 
 # View the result
-summary(lm_1)
+head(predict_score_df)
 
+# Create a linear regression model
+score_model <- lm(ACT ~ age + education + SATV + SATQ,
+                  data = predict_score_df)
 
-# -------------------------------------------------------
-
-
-# Perform a mulitple linear regression analysis
-lm_2 <- lm(sleep ~ exercise + caffeine + screen_time, data = sleep_df)
-
-# View the result
-summary(lm_2)
+# View the model summary
+summary(score_model)
